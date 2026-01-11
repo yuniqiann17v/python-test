622891de13e4a06dc9552849f7f557f4878f8c20
Sawyer Mitchell <hgdatawm3363@outlook.com>
2026-01-12 04:31:38 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+    
+    def multiply(self, a, b):
+        return a * b
+
+if __name__ == "__main__":
+    calc = Calculator()
+    print(calc.add(5, 3))
