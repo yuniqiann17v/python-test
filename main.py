7fd6a69f11563ef9b8a2a9bf91c5ee08485e2e92
Blair Watson <hgdatawm3363@outlook.com>
2026-01-12 17:18:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-class Calculator:
-    def add(self, a, b):
-        return a + b
-    
-    def multiply(self, a, b):
-        return a * b
+# Last updated: 2026-01-12 17:18:03
 
-if __name__ == "__main__":
-    calc = Calculator()
-    print(calc.add(5, 3))
+class Calculator:    def add(self, a, b):        return a + b        def multiply(self, a, b):        return a * bif __name__ == "__main__":    calc = Calculator()    print(calc.add(5, 3))
